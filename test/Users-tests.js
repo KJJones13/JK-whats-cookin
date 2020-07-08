27bec90c88f79b7bf86b063b651076a33eeac809
Jake West <jacobkwest93@gmail.com>
2020-07-07 20:51:19 -0600

    Add User tests

--- test/Users-tests.js
+++ test/Users-tests.js
@@ -1,11 +1,14 @@
 const chai = require("chai");
 const expect = chai.expect;
-const Users = require("../src/users")
+const Users = require("../src/users");
+const 
 
-describe ("Users"), function() {
+describe ("Users", function() {
+  let user, recipe;
   beforeEach(function () {
     //Describe variables used in test here!
-    const user = new Users();
+    user = new Users();
+    recipe =
       // let deck, round, game;
       //     beforeEach(function () {
         //       game = new Game();
@@ -18,4 +21,21 @@ describe ("Users"), function() {
   it ("should be a function", function() {
     expect (Users).to.be.a("function");
   });
-}
+
+  it ("should be an instance of Users", function() {
+    expect (user).to.be.an.instanceof(Users)
+  });
+
+  it ("should start with an empty favorites list", function() {
+    expect (user.favoriteRecipes).to.deep.equal([])
+  })
+
+  it ("should be able to add favorite recipes to a list", function() {
+    //1
+
+    //2
+
+    //3
+    expect(user.addRecipe())
+  })
+})
