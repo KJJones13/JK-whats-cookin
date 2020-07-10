8d13d81260817d7bea5902ee0b1880cd1fca1e80
Kyle Jones <rmbart395@yahoo.com>
2020-07-09 18:44:35 -0600

    Add code to scripts from main.js and removed main.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+// const Users = require("../src/users");
+
+const favRecipeBtn = document.querySelector(".favorite-recipes-btn");
+
+favRecipeBtn.addEventListener("click", showMessage);
+let user = new Users();
+
+function showMessage() {
+  alert(user);
+  return user;
+}
