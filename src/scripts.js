f56acdd3380349fc4c798afefa54b1b2380c669d
Kyle Jones <rmbart395@yahoo.com>
2020-07-10 13:53:38 -0600

    Add code to hook up the buttons #36

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,13 @@
-// const Users = require("../src/users");
-
 const favRecipeBtn = document.querySelector(".favorite-recipes-btn");
+const groceryListBtn = document.querySelector(".grocery-list-btn");
+const usersBtn = document.querySelector(".users-btn");
+const saveToBoxBtn = document.querySelector(".save-to-box-btn");
 
 favRecipeBtn.addEventListener("click", showMessage);
+groceryListBtn.addEventListener("click", showMessage);
+usersBtn.addEventListener("click", showMessage);
+saveToBoxBtn.addEventListener("click", showMessage);
+
 let user = new Users();
 
 function showMessage() {
