421e4827de22865c2f6d49f5e400613f33a92b4e
Jake West <jacobkwest93@gmail.com>
2020-07-11 13:30:58 -0600

    Add drop-down on main page; re-styled recipe card

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,33 @@
 const favRecipeBtn = document.querySelector(".favorite-recipes-btn");
 const groceryListBtn = document.querySelector(".grocery-list-btn");
 const usersBtn = document.querySelector(".users-btn");
-const saveToBoxBtn = document.querySelector(".save-to-box-btn");
+const saveBtn = document.querySelector(".save-btn");
 
-favRecipeBtn.addEventListener("click", showMessage);
-groceryListBtn.addEventListener("click", showMessage);
-usersBtn.addEventListener("click", showMessage);
-saveToBoxBtn.addEventListener("click", showMessage);
+window.addEventListener('load', openRecipeInfo)
+// favRecipeBtn.addEventListener("click", showMessage);
+// groceryListBtn.addEventListener("click", showMessage);
+// usersBtn.addEventListener("click", showMessage);
+// saveBtn.addEventListener("click", showMessage);
 
-let user = new Users();
 
-function showMessage() {
-  alert(user);
-  return user;
+// let user = new Users();
+//
+// function showMessage() {
+//   alert(user);
+//   return user;
+// }
+
+function openRecipeInfo() {
+  const coll = document.getElementsByClassName("collapsible");
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display=== "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 }
