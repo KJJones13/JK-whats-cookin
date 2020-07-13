5825f0554be77a0db1f54d5044d7e58e466499d9
Kyle Jones <rmbart395@yahoo.com>
2020-07-13 15:52:14 -0600

    Add code to display the users search input #32

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,7 @@ const userView = document.querySelector(".user-view");
 const favoriteView = document.querySelector(".favorites-view")
 const favoriteRecipeGrid = document.querySelector(".favorite-recipes-grid");
 const groceryView = document.querySelector(".groceryList-view");
+const searchView = document.querySelector(".search-results-view");
 /*-----------Buttons-----------*/
 const favRecipeBtn = document.querySelector(".favorite-recipes-btn");
 const groceryListBtn = document.querySelector(".grocery-list-btn");
@@ -24,7 +25,7 @@ homeBtn.addEventListener("click", changeView);
 favRecipeBtn.addEventListener("click", changeView);
 groceryListBtn.addEventListener("click", changeView);
 usersBtn.addEventListener("click", changeView);
-searchBtn.addEventListener("click", searchAllRecipes);
+searchBtn.addEventListener("click", changeView);
 
 /*-----------Page Load Functions-----------*/
 function openRecipeInfo() {
@@ -120,8 +121,52 @@ function addClickToSaveButton() {
 /*-----------Search-Related Functions-----------*/
 function searchAllRecipes() {
   event.preventDefault();
-  console.log(currentUser.searchRecipes(searchBar.value))
-  currentUser.searchRecipes(searchBar.value);
+  return currentUser.searchRecipes(searchBar.value)
+}
+
+function buildResultsSection(recipes) {
+  recipes.forEach(recipe => {
+    let newRecipeCard = `
+    <section class="recipe-card" id=${recipe.id}>
+      <section class="card-head">
+        <img class="card-image" src=${recipe.image}>
+        <button type="button" class="save-btn" id=${recipe.id}>Save Recipe</button>
+      </section>
+      <section class="card-body">
+        <button type="button" class="collapsible">${recipe.name}</button>
+        <section class="content">
+          <section class="ingredients">
+            <p>Ingredients</p>
+            <ul>
+              <li>1.5 c 20081</li>
+              <li>0.5 tsp 18372</li>
+              <li>1 large 1123</li>
+              <li>0.5 c 19335</li>
+              <li>3 Tbsp 19206</li>
+              <li>0.5 c 19334</li>
+              <li>0.5 tsp 2047</li>
+              <li>24 servings 1012047</li>
+              <li>2 c 10019903</li>
+              <li>0.5 c 1145</li>
+              <li>0.5 tsp 2050</li>
+            </ul>
+          </section>
+          <section class="instructions">
+            <p>Instructions<p>
+            <ol>
+              <li>In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.</li>
+              <li>Add egg and vanilla and mix until combined.</li>
+              <li>Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.</li>
+              <li>Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.</li>
+              <li>Bake for 9 to 10 minutes, or until you see the edges start to brown.</li>
+              <li>Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.</li>
+            </ol>
+          </section>
+        </section>
+      </section>
+    `;
+    return searchView.insertAdjacentHTML('beforeend', newRecipeCard);
+  });
 }
 // create a function to return recipes that match the users input
 //
@@ -133,23 +178,34 @@ function changeView(event) {
     userView.classList.remove('hidden');
     favoriteView.classList.add('hidden');
     groceryView.classList.add('hidden');
+    searchView.classList.add('hidden');
     displayUsers();
   } else if (event.target.className === "favorite-recipes-btn") {
       homeView.classList.add('hidden');
       userView.classList.add('hidden');
       favoriteView.classList.remove('hidden');
       groceryView.classList.add('hidden');
+      searchView.classList.add('hidden');
       displayFavoriteRecipes();
   } else if (event.target.className === "grocery-list-btn") {
       homeView.classList.add('hidden');
       userView.classList.add('hidden');
       favoriteView.classList.add('hidden');
       groceryView.classList.remove('hidden');
+      searchView.classList.add('hidden');
   } else if (event.target.className === "home-btn") {
       homeView.classList.remove('hidden');
       userView.classList.add('hidden');
       favoriteView.classList.add('hidden');
       groceryView.classList.add('hidden');
+      searchView.classList.add('hidden');
+  } else if (event.target.className === "search-btn") {
+    homeView.classList.add('hidden');
+    userView.classList.add('hidden');
+    favoriteView.classList.add('hidden');
+    groceryView.classList.add('hidden');
+    searchView.classList.remove('hidden');
+    buildResultsSection(searchAllRecipes());
   }
 }
 
