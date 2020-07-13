63296d4aa4d773599fc88e5c751a3e67bca76f4f
Jake West <jacobkwest93@gmail.com>
2020-07-13 17:55:29 -0600

    Add map functions to import recipe specific info to home screen

--- src/scripts.js
+++ src/scripts.js
@@ -74,28 +74,18 @@ function buildHomeView() {
           <section class="ingredients">
             <p>Ingredients</p>
             <ul>
-              <li>1.5 c 20081</li>
-              <li>0.5 tsp 18372</li>
-              <li>1 large 1123</li>
-              <li>0.5 c 19335</li>
-              <li>3 Tbsp 19206</li>
-              <li>0.5 c 19334</li>
-              <li>0.5 tsp 2047</li>
-              <li>24 servings 1012047</li>
-              <li>2 c 10019903</li>
-              <li>0.5 c 1145</li>
-              <li>0.5 tsp 2050</li>
+              ${recipe.ingredients.map(ingredient => {
+                return `<li>${ingredient.quantity.amount} ${ingredient.quantity.unit} of ${ingredient.id}</li>`
+                })
+              }
             </ul>
           </section>
           <section class="instructions">
             <p>Instructions<p>
             <ol>
-              <li>In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.</li>
-              <li>Add egg and vanilla and mix until combined.</li>
-              <li>Add dry ingredients and mix on low just until incorporated. Stir in chocolate chips.Scoop the dough into 1,5 tablespoon size balls and place on a plate or sheet. Cover with saran wrap and chill at least 2 hours or overnight.When ready to bake, preheat oven to 350 degrees.</li>
-              <li>Place the cookie dough balls into ungreased muffin pan. Sprinkle with sea salt.</li>
-              <li>Bake for 9 to 10 minutes, or until you see the edges start to brown.</li>
-              <li>Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.</li>
+              ${recipe.instructions.map(instruction => {
+                return `<li>${instruction.instruction}</li>`
+              })}
             </ol>
           </section>
         </section>
